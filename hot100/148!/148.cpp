60a39678d0b92a0bd3b933c820c111333002fe61
jambow <1193785411@qq.com>
2025-10-19 11:24:06 +0800

    148翻转链表额外空间O(1)

--- hot100/148!/148.cpp
+++ hot100/148!/148.cpp
@@ -1,3 +1,7 @@
+// 翻转链表，因为链表的性质，所以不适合使用swap的快排，只能用归并
+// 但是归并要递归实现的话，额外空间是log级别（栈空间）
+// 所以只能选择从底层合并上来的归并排序方法，这样额外空间是O(1)级别
+
 struct ListNode
 {
     int val;
@@ -15,16 +19,66 @@ public:
         if(!head) return head;
 
         int len = 0;
-        ListNode* u = head;
-        while(!u)
+        ListNode *curr = head;
+        while(curr)
         {
             ++len;
-            u = u->next;
+            curr = curr->next;
         }
 
-        for(int i = 0; (1 << i) < len; ++i)
+        ListNode dummyHead = ListNode(0, head);
+        for(int sublen = 1; sublen < len; sublen <<= 1)
         {
-            ;
+            ListNode *prev = &dummyHead; 
+            ListNode *curr = dummyHead.next;
+
+            while(curr)
+            {
+                ListNode *head1 = curr;
+                ListNode *head2 = split(head1, sublen);
+                curr = split(head2, sublen);
+                prev = merge(head1, head2, prev);
+            }
         }
+
+        return dummyHead.next;
+    }
+
+private:
+    ListNode *split(ListNode *head, int len)
+    {
+        while(head && --len)
+        {
+            head = head->next;
+        }
+        if(!head) return nullptr;
+        ListNode *nexthead = head->next;
+        head->next = nullptr;
+        return nexthead;
+    }
+
+    ListNode *merge(ListNode *head1, ListNode *head2, ListNode *prev)
+    {
+        ListNode *curr = prev;
+
+        while(head1 && head2)
+        {
+            if(head1->val <= head2->val)
+            {
+                curr->next = head1;
+                head1 = head1->next;
+            }
+            else
+            {
+                curr->next = head2;
+                head2 = head2->next;
+            }
+            curr = curr->next;
+        }
+
+        curr->next = head1 ? head1 : head2;
+        while(curr->next) curr = curr->next;
+
+        return curr;
     }
 };
