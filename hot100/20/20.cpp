b350de9decda6a19a378f1a0fb17b776352e6665
jambow <1193785411@qq.com>
2025-11-13 10:11:22 +0800

    20有效的括号

--- hot100/20/20.cpp
+++ hot100/20/20.cpp
@@ -1,19 +1,49 @@
 #include <string>
+#include <stack>
 
 using std::string;
+using std::stack;
 
 class Solution {
 public:
     bool isValid(string s) {
         int cnt1 = 0, cnt2 = 0, cnt3 = 0;
+        stack<char> sta;
 
         for(auto c : s)
         {
-            if(c == '(') ++cnt1;
-            if(c == '[') ++cnt2;
-            if(c == '{') ++cnt3;
+            if(c == ')') 
+            {
+                if(sta.empty()) return false;
+                else
+                {
+                    if(sta.top() != '(') return false;
+                    sta.pop();
+                }
+            }
+            else if(c == ']')
+            {
+                if(sta.empty()) return false;
+                else
+                {
+                    if(sta.top() != '[') return false;
+                    sta.pop();
+                }
+            }
+            else if(c == '}')
+            {
+                if(sta.empty()) return false;
+                else
+                {
+                    if(sta.top() != '{') return false;
+                    sta.pop();
+                }
+            }
+            else sta.push(c);
         }
 
-        return true;
+        if(sta.empty())
+            return true;
+        else return false;
     }
 };
